c3090b6089fbbb0a1a455003201b2be619e36de6
Jimmy <me@jimmymorales.dev>
2020-08-02 15:34:18 -0600

    Finish LJF logic

--- src/app/ljf/ljf.component.ts
+++ src/app/ljf/ljf.component.ts
@@ -30,7 +30,7 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
       transition('getting => waiting', [
         animate('1s')
       ]),
-      transition('waiting => viewing', [
+      transition('waiting <=> viewing', [
         animate('1s')
       ]),
       transition('viewing => getting', [
@@ -79,6 +79,11 @@ export class LjfComponent implements OnInit, OnDestroy {
   ];
   displayedColumns: string[] = ['name', 'd1', 'd2', 'total'];
 
+  private timeout1 = 0;
+  private timeout2 = 0;
+  private timeout3 = 0;
+  private timeout4 = 0;
+
   constructor(private schedulingCalculatorService: SchedulingParametersStore) {
   }
 
@@ -93,24 +98,33 @@ export class LjfComponent implements OnInit, OnDestroy {
   }
 
   newState(event: AnimationEvent): void {
+    console.log('new state', event);
     // @ts-ignore
     if (event.triggerName === 'video1') {
       // @ts-ignore
       if (event.toState === 'getting') {
-        setTimeout(() => {
+        this.timeout1 = setTimeout(() => {
           this.video1 = 'waiting';
-        }, 2499);
+        }, (this.schedulingCalculatorService.BASE_TIME / this.schedulingCalculatorService.lambda) - 10);
       } else { // @ts-ignore
         if (event.toState === 'waiting') {
           if (this.video2 !== 'viewing') {
             this.video1 = 'viewing';
+          } else if (this.schedulingCalculatorService.d1 > this.schedulingCalculatorService.d2) {
+            if (this.video2 === 'viewing') {
+              clearTimeout(this.timeout4);
+              this.video2 = 'waiting';
+            }
+            this.video1 = 'viewing';
           }
         } else { // @ts-ignore
           if (event.toState === 'viewing') {
-            setTimeout(() => {
+            this.timeout2 = setTimeout(() => {
               this.video1 = 'getting';
-              this.video2 = 'viewing';
-            }, 10000);
+              if (this.video2 === 'waiting') {
+                this.video2 = 'viewing';
+              }
+            }, this.schedulingCalculatorService.BASE_TIME * this.schedulingCalculatorService.d1);
           }
         }
       }
@@ -118,20 +132,28 @@ export class LjfComponent implements OnInit, OnDestroy {
       if (event.triggerName === 'video2') {
         // @ts-ignore
         if (event.toState === 'getting') {
-          setTimeout(() => {
+          this.timeout3 = setTimeout(() => {
             this.video2 = 'waiting';
-          }, 2501);
+          }, (this.schedulingCalculatorService.BASE_TIME / this.schedulingCalculatorService.lambda) + 10);
         } else { // @ts-ignore
           if (event.toState === 'waiting') {
             if (this.video1 !== 'viewing') {
               this.video2 = 'viewing';
+            } else if (this.schedulingCalculatorService.d2 > this.schedulingCalculatorService.d1) {
+              if (this.video1 === 'viewing') {
+                clearTimeout(this.timeout2);
+                this.video1 = 'waiting';
+              }
+              this.video2 = 'viewing';
             }
           } else { // @ts-ignore
             if (event.toState === 'viewing') {
-              setTimeout(() => {
-                this.video1 = 'viewing';
+              this.timeout4 = setTimeout(() => {
+                if (this.video1 === 'waiting') {
+                  this.video1 = 'viewing';
+                }
                 this.video2 = 'getting';
-              }, 6666);
+              }, this.schedulingCalculatorService.BASE_TIME * this.schedulingCalculatorService.d2);
             }
           }
         }
@@ -145,29 +167,33 @@ export class LjfComponent implements OnInit, OnDestroy {
     const E1 = lambda;
     const E2 = 1 / d2;
     const E3 = 1 / d1;
-    // tslint:disable-next-line:max-line-length
-    const pvw = (E1 * E1 * E2 * (2 * E1 + E2)) / (2 * (E1 * E1 * E1) * E3 + 2 * (E1 * E1 * E1) * E2 + E1 * E1 * E3 * E3 + 4 * (E1 * E1) * E2 * E3 + E1 * E1 * E2 * E2 + 2 * E1 * E3 * E3 * E2 + 2 * E1 * E3 * E2 * E2 + E2 * E2 * E3 * E3);
-    // tslint:disable-next-line:max-line-length
-    const pvg = (E1 * E3 * E2 * (2 * E1 + E2)) / (2 * (E1 * E1 * E1) * E3 + 2 * (E1 * E1 * E1) * E2 + E1 * E1 * E3 * E3 + 4 * (E1 * E1) * E2 * E3 + E1 * E1 * E2 * E2 + 2 * E1 * E3 * E3 * E2 + 2 * E1 * E3 * E2 * E2 + E2 * E2 * E3 * E3);
+    const pvg = (E1 * E2 * E3) / (2 * E1 * E1 * E1 + 3 * E1 * E1 * E3 + E1 * E1 * E2 + E1 * E3 * E3 + 2 * E1 * E2 * E3 + E3 * E3 * E2);
     const pgg = (E2 * E3) / (2 * E1 * E1 + E1 * E3 + E1 * E2 + E2 * E3);
     // tslint:disable-next-line:max-line-length
-    const pgv = (E1 * E2 * E3 * (2 * E1 + E3)) / (2 * (E1 * E1 * E1) * E3 + 2 * (E1 * E1 * E1) * E2 + E1 * E1 * E3 * E3 + 4 * (E1 * E1) * E2 * E3 + E1 * E1 * E2 * E2 + 2 * E1 * E3 * E3 * E2 + 2 * E1 * E3 * E2 * E2 + E2 * E2 * E3 * E3);
+    const pgv = (E1 * E3 * (2 * E1 + E3)) / (2 * E1 * E1 * E1 + 3 * E1 * E1 * E3 + E1 * E1 * E2 + E1 * E3 * E3 + 2 * E1 * E2 * E3 + E3 * E3 * E2);
     // tslint:disable-next-line:max-line-length
-    const pwv = (E1 * E1 * E3 * (2 * E1 + E3)) / (2 * (E1 * E1 * E1) * E3 + 2 * (E1 * E1 * E1) * E2 + E1 * E1 * E3 * E3 + 4 * (E1 * E1) * E2 * E3 + E1 * E1 * E2 * E2 + 2 * E1 * E3 * E3 * E2 + 2 * E1 * E3 * E2 * E2 + E2 * E2 * E3 * E3);
+    const pvw = (E1 * E1 * (2 * E1 + E2 + E3)) / (2 * E1 * E1 * E1 + 3 * E1 * E1 * E3 + E1 * E1 * E2 + E1 * E3 * E3 + 2 * E1 * E2 * E3 + E3 * E3 * E2);
 
     const u1 = pvw + pvg;
-    const u2 = pgv + pwv;
+    const u2 = pgv;
 
     this.dataSource[0].d1 = u1 / d1;
     this.dataSource[0].d2 = u2 / d2;
     this.dataSource[0].total = this.dataSource[0].d1 + this.dataSource[0].d2;
 
-    this.dataSource[1].d1 = pvw + pvg + pwv;
-    this.dataSource[1].d2 = pvw + pgv + pwv;
+    this.dataSource[1].d1 = pvw + pvg;
+    this.dataSource[1].d2 = pvw + pgv;
     this.dataSource[1].total = this.dataSource[1].d1 + this.dataSource[1].d2;
 
     this.dataSource[2].d1 = this.dataSource[1].d1 / this.dataSource[0].d1;
     this.dataSource[2].d2 = this.dataSource[1].d2 / this.dataSource[0].d2;
     this.dataSource[2].total = this.dataSource[1].total / this.dataSource[0].total;
+
+    clearTimeout(this.timeout1);
+    clearTimeout(this.timeout2);
+    clearTimeout(this.timeout3);
+    clearTimeout(this.timeout4);
+    this.video1 = 'getting';
+    this.video2 = 'getting';
   }
 }
